8cf424d771dbeeb0cd904ba50962dc187f73a8bf
翔 <297962650@qq.com>
2019-09-23 00:49:24 +0800

    首页商品楼层

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,19 @@
 Page({
   data : {
+    // 轮播图数组
     swiperList : [],
-    catItems : []
+    // 导航数组
+    catItems : [],
+    // 楼层数组
+    floorList : []
   },
+
   onLoad(){
     this.getSwiperData()
     this.getCatItems()
+    this.getFloorData()
   },
+
   // 获取轮播图数据
   getSwiperData(){
     wx.request({
@@ -20,14 +27,27 @@ Page({
     });
   },
 
-  // 获取首页导航
+  // 获取首页导航数组
   getCatItems(){
       wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
       success: (result) => {
-        console.log(result)
+        // console.log(result)
         this.setData({
           catItems : result.data.message
+        })
+      }
+    });
+  },
+
+  // 获取首页楼层数组
+  getFloorData(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success: (result) => {
+        console.log(result)
+        this.setData({
+          floorList : result.data.message
         })
       }
     });
